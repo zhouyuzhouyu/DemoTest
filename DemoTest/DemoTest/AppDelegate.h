681c346674a43ef55dd6b40dbddb22f7e7b2cbc8
周玉 <zhouyu@zhouyudeMac-mini-2.local>
2014-05-14 13:37:02 +0800

    init

---
+++ DemoTest/DemoTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoTest
+//
+//  Created by 周玉 on 14-5-14.
+//  Copyright (c) 2014年 huoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoTest/DemoTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoTest
+//
+//  Created by 周玉 on 14-5-14.
+//  Copyright (c) 2014年 huoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
